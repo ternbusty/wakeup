a7eb7f3d8ad99b48810ae58edbb176374ff16ef0
walking_trashbox <a.iwasaki0128@gmail.com>
2020-01-04 22:27:42 +0900

    initial commit

---
+++ rotate.py
@@ -0,0 +1,41 @@
+import RPi.GPIO as GPIO
+import time
+import sys
+
+def init_gpio():
+    GPIO.setmode(GPIO.BCM)
+    channel_list = [24, 25]
+    GPIO.setup(channel_list, GPIO.OUT)
+
+def stop():
+    GPIO.output(24, False)
+    GPIO.output(25, False)
+
+def cleanup():
+    GPIO.cleanup()
+
+def rotate_right(duration):
+    init_gpio()
+    stop()
+    GPIO.output(24, True)
+    GPIO.output(25, False)
+    time.sleep(duration)
+    stop()
+    cleanup()
+
+def rotate_left(duration):
+    init_gpio()
+    stop()
+    GPIO.output(24, False)
+    GPIO.output(25, True)
+    time.sleep(duration)
+    stop()
+    cleanup()
+
+if __name__ == '__main__':
+    args = sys.argv
+    if args[1] == 'lock':
+        rotate_right(5)
+    elif args[1] == 'unlock':
+        rotate_left(5)
+
